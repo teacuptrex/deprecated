b2b4d30f46d01799936cfb96121f970361722b9d
jayme <jaymeipema@gmail.com>
2021-01-19 16:20:40 -0500

    initial merged commit

---
+++ src/main/java/co/personal/personalwebsite/PersonalwebsiteApplication.java
@@ -0,0 +1,13 @@
+package co.personal.personalwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalwebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalwebsiteApplication.class, args);
+	}
+
+}
